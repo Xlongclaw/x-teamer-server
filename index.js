1de4cbfc8f8ef018e779a3b0b4c81d57a245d8ca
unknown <xlongclaw@gmail.com>
2024-04-02 17:36:12 +0530

    refactor(config): Remove hardcoded port and add it to dotenv configuration

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv")
 dotenv.config()
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`Server running at port: ${PORT}`);

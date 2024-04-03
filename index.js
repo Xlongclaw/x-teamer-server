9a2848d59eb0b3deab6b1c8a28dfbd282c40e6ae
unknown <xlongclaw@gmail.com>
2024-04-04 01:12:01 +0530

    refactor(docs): add docs and remove extra code _FULL_

--- index.js
+++ index.js
@@ -1,22 +1,40 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const cors = require('cors')
+const cors = require('cors');
 const baseRouter = require("./router/base-router");
 const apiRouter = require("./router/api-router");
 const connectDB = require("./database/connectDB");
 const bodyParser = require("body-parser");
+
 dotenv.config();
+
 const app = express();
 const PORT = process.env.PORT;
 
+/**
+ * Express application for handling HTTP requests.
+ * @module server
+ */
 
-app.use(cors())
-app.use(bodyParser.json())
+// Enable Cross-Origin Resource Sharing (CORS)
+app.use(cors());
 
-app.use("/",baseRouter);
-app.use("/api",apiRouter);
+// Parse incoming request bodies in JSON format
+app.use(bodyParser.json());
 
-app.listen(PORT,async () => {
+// Routes for base URL and API
+app.use("/", baseRouter);
+app.use("/api", apiRouter);
+
+/**
+ * Starts the server and listens on the specified port.
+ * @name Listen
+ * @function
+ * @memberof module:server
+ * @param {number} PORT - The port number on which the server will listen.
+ */
+app.listen(PORT, async () => {
   console.log(`🟢 Server running at port: ${PORT}`);
+  // Connect to the database
   connectDB();
 });
--- router/base-router/index.js
+++ router/base-router/index.js
@@ -1,5 +1,18 @@
+/**
+ * Express router for handling base routes.
+ * @module baseRouter
+ */
+
 const router = require("express").Router();
 
+/**
+ * Route serving the root endpoint.
+ * @name GET /
+ * @function
+ * @memberof module:baseRouter
+ * @param {Object} _ - Express request object (unused).
+ * @param {Object} res - Express response object.
+ */
 router.route("/").get((_, res) => {
   res.send("X-Teamer Server Running");
 });
